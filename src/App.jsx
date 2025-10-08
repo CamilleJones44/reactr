ac8abf578a55715be838b70b3f1dfc72aeb0c45c
CamilleJones44 <danhumprey123@gmail.com>
2025-10-08 18:19:54 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,49 @@
-import React from 'react';
+import { useState } from 'react'
 
-const items = ['Apples', 'Bananas', 'Cherries', 'Grapes'];
+import './App.css'
 
 
-function Header() {
-  
-  return <h1>My Grocery List y</h1>;
-}
 
-function List() {
-  
-  return (
-    <ul>
-      
-      {items.map((item, index) => (
-      
-        <li key={index}>{item}</li>
-      
-      ))}
-
-      
-    </ul>
-    
-  );
-}
 
 
 function App() {
-  
+  const [count, setCount] = useState(0)
+
   return (
     <div>
-      
-      <Header />
-      
-      <List />
-      <List />
-      
+      <h1> Welcome to the counter App</h1>
+
+      <Counter title = "My Counter App"/>
+
     </div>
-  );
+  )
+}
+function Counter({title}) {
+  // you'll need to add state and props here.
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    // This function should increase the count
+    setCount(count + 1);
+   
+  }
+
+
+  const handleReset = () => {
+    // This function should increase the count
+    setCount(0);
+  
+  }
+
+  return (
+    <div>
+
+      <h2>{title}</h2>
+      <p>Count:{count}</p>
+      <button onClick={handleIncrease}>increase</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
